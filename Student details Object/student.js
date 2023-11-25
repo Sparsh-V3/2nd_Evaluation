a1ff7309443f25a5dc62a91fe070cc8b3ca82a21
unknown <vardhansparsh46@gmail.com>
2023-11-25 20:07:50 +0530

    Second prpblem commited

---
+++ Student details Object/student.js
@@ -0,0 +1,15 @@
+
+function StudentDetailsObject(name, roll, clss, section, science, maths, social_science, english, hindi){
+    this.name = name;
+    this.roll = roll;
+    this.clss = clss;
+    this.section = section; 
+    {
+        this.science = science;
+        this.maths = maths;
+        this.social_science = social_science;
+        this.english = english;
+        this.hindi = hindi;
+    };
+}
+
